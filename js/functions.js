ca87f3df6b98ec02501f28a096b49fc7d8fa08fd
Juli Ramon <juli@nexumlabs.com>
2020-11-11 18:02:27 +0100

    fixed typo

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,3 @@
-let getYear = () => {
-    let year = new Date().getFullYear();
-    let stamp = document.querySelector('#timeStamp');
-    stamp.innerHTML = year;
-}
-
 $(window).on('load', function () {
     $('#overlay').fadeOut();
-    getYear();
 });
